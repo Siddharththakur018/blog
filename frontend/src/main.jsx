4ea334d462ad097afc1e67b64c3cd18bf0e842f8
Siddharth Thakur <siddharththakur@Siddharths-MacBook-Air.local>
2025-07-19 16:34:17 +0530

    added render file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/authContext.jsx'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
