c70ab2dcaf1174c3ad3ffc05ddc108ca66ef0a40
Siddharth Thakur <siddharththakur@Siddharths-MacBook-Air.local>
2025-07-19 12:31:42 +0530

    changed bcrypt to bcryptjs

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const saltFactor = 10;
 const User = require('../model/userModel');
